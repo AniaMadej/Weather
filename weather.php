74e9237acecf201a584a8f1db3f2114681d6d5e9
Anna Madej <aniamadej123@gmail.com>
2017-01-26 19:38:19 +0100

    weather

---
+++ weather.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use AMadej\Weather\CurrentWeather;
+
+$city = $argv[1];
+$apiKey = 'af319cd969dff7d8c42768f6f0d8c979';
+$http = new Client();
+
+$weatherApi = new CurrentWeather($http, $apiKey);
+$temp = $weatherApi->getTemperature($city);
+
+echo sprintf("Temperature in %s is %s Celsious \n", $city, $temp);
